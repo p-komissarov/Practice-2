44de966adf5313ef30ca247f7d75e1d0f5eb7660
p-komissarov <platon.komissaroff@gmail.com>
2025-02-17 01:12:04 +0700

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,3 +1,3 @@
 total=float(input("Введите значение общей стоимости часов: "))
-pricegold=round((total - 96 * 48) / (96 / 16))
+pricegold=int((total - 96 * 48) / (96 / 16))
 print(f"Стоимость золотых часов: {pricegold}")
